fb3ee2ae805652158cb2494e08674dc677dc5354
Fahmaz-penoft <fahmaz.ashraf@penoft.com>
2026-07-30 10:48:17 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Branch tutorial!\n");
+    printf("my new code will go here - this is new git code\n");
+    printf("Your new code will go here\n");
+    return 0;
+}
