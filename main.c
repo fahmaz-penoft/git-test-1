829b36ea5c944593b754c5787ba4c8abee57d088
Fahmaz-penoft <fahmaz.ashraf@penoft.com>
2026-07-30 12:19:38 +0530

    conflict check for thridbranch

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(void) {
     printf("Branch tutorial!\n");
     printf("my new code will go here - this is new git code\n");
-    printf("updated the main.c file for git - new updated branch of my_nxt_branch");
+    printf("updated the main.c file for git - new updated branch of third-branch");
     return 0;
 }
