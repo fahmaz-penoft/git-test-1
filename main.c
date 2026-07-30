f569658f172668a7fbe97300b319f0c92de29b05
Fahmaz-penoft <fahmaz.ashraf@penoft.com>
2026-07-30 12:14:35 +0530

    conflict check for my_nxt_branch

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(void) {
     printf("Branch tutorial!\n");
     printf("my new code will go here - this is new git code\n");
-    printf("updated the main.c file for git");
+    printf("updated the main.c file for git - new updated branch of my_nxt_branch");
     return 0;
 }
